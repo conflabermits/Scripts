5296acb76de8359bb09948fca6e45f1580a37ea9
Chris Dunaj <chris@MacBookPro.local>
2016-08-20 11:15:01 -0400

    Added OS check

--- testSay.sh
+++ testSay.sh
@@ -3,9 +3,17 @@
 # Uses the Mac say command to speak via the speakers
 # Mostly just for testing out bash command syntax
 
+if [ $(uname) == "Darwin" ] ; then
+	echo
+else
+	echo "This script works best on Mac OS X!"
+	exit
+fi
+
 echo "What would you like me to say?"
 read userInput
 
 say $userInput
 
+echo
 
