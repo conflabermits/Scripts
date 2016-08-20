4eb98a6d1a15c6de15efdaa1cadbe9a413c6b489
Chris Dunaj <chris@MacBookPro.local>
2016-08-20 10:12:50 -0400

    Added another comment to testSay

--- testSay.sh
+++ testSay.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#Uses the Mac say command to speak via the speakers
+# Uses the Mac say command to speak via the speakers
+# Mostly just for testing out bash command syntax
 
 echo "What would you like me to say?"
 read userInput
