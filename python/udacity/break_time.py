a47590fc36dfd4340be784b66b73a00146f7f9a5
Christopher Dunaj <Christopher Dunaj>
2017-02-04 19:56:24 -0500

    Made break time operational, added sleep timer, loop, and print statements

--- python/udacity/break_time.py
+++ python/udacity/break_time.py
@@ -1,1 +1,13 @@
-webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime())
+while break_count < total_breaks:
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=djV11Xbc914")
+    print("Break "+str(break_count + 1)+" started at "+time.ctime())
+    break_count += 1
+
