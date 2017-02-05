d0d5d1db333905473d5802c17b9d47f5bdf7fcf9
Christopher Dunaj <Christopher Dunaj>
2017-02-04 20:06:01 -0500

    Added sleep_time as variable

--- python/udacity/break_time.py
+++ python/udacity/break_time.py
@@ -3,10 +3,11 @@ import webbrowser
 
 total_breaks = 3
 break_count = 0
+sleep_time = 10  # Set to 720 for two hour breaks
 
 print("This program started on "+time.ctime())
 while break_count < total_breaks:
-    time.sleep(5)
+    time.sleep(sleep_time)
     webbrowser.open("https://www.youtube.com/watch?v=djV11Xbc914")
     print("Break "+str(break_count + 1)+" started at "+time.ctime())
     break_count += 1
