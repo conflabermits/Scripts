284986d82448c7b132123f5a80e1e8e2c7f6ce8e
conflabermits <christopher.dunaj@gmail.com>
2017-04-01 16:39:48 -0400

    Added draw_square and movies.

---
+++ python/udacity/movies.py
@@ -0,0 +1,10 @@
+title
+storyline
+
+def short_trailer(title, youtube_url):
+    # Open browser and play trailer
+
+def show_info(title, storyline, release_date
+              rating, youtube_url):
+    # Print movie information
+
