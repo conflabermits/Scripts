a2cb2f01a2ffd7bba898ad1a37a4c52b68a77b8f
conflabermits <christopher.dunaj@gmail.com>
2017-05-05 14:59:49 -0400

    Added boondock saints to EC

--- python/udacity/movies/entertainment_center.py
+++ python/udacity/movies/entertainment_center.py
@@ -9,5 +9,10 @@ avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-print(avatar.storyline)
-avatar.show_trailer()
+#print(avatar.storyline)
+#avatar.show_trailer()
+boondock_saints = media.Movie("Boondock Saints",
+                              "Fraternal twins set out to rid Boston of the evil men operating there",
+                              "https://upload.wikimedia.org/wikipedia/en/1/1b/The_Boondock_Saints_poster.jpeg",
+                              "https://www.youtube.com/watch?v=ydXojYfCF3I")
+boondock_saints.show_trailer()
