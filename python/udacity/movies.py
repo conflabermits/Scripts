34bad4b33bd384c55146cb6e3870f38feea4b6d0
conflabermits <christopher.dunaj@gmail.com>
2017-05-05 14:08:32 -0400

    Slight formatting changes to movies.py, plus full check_profanity script with added if for if loop to return individual curse words.

--- python/udacity/movies.py
+++ python/udacity/movies.py
@@ -4,7 +4,6 @@ storyline
 def short_trailer(title, youtube_url):
     # Open browser and play trailer
 
-def show_info(title, storyline, release_date
-              rating, youtube_url):
+def show_info(title, storyline, release_date, rating, youtube_url, director, box_office):
     # Print movie information
 
