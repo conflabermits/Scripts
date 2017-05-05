392e4c246cb190b38ab18dc68afc987a287dc76c
conflabermits <christopher.dunaj@gmail.com>
2017-05-05 14:49:40 -0400

    Updating entertainment_center with avatar movie object

--- python/udacity/movies/entertainment_center.py
+++ python/udacity/movies/entertainment_center.py
@@ -4,4 +4,9 @@ toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://youtube.com/watch?v=vwyZH85NQC4")
-print(toy_story.storyline)
+#print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+print(avatar.storyline)
