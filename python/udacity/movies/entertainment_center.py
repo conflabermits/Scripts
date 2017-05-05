885025f4ed131c61721082b26d4aab4369db87b4
conflabermits <christopher.dunaj@gmail.com>
2017-05-05 14:56:01 -0400

    Added show_trailer instance function.

--- python/udacity/movies/entertainment_center.py
+++ python/udacity/movies/entertainment_center.py
@@ -10,3 +10,4 @@ avatar = media.Movie("Avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 print(avatar.storyline)
+avatar.show_trailer()
