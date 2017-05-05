a6a94f1afccb6013f7c411719779d731a31a0cee
conflabermits <christopher.dunaj@gmail.com>
2017-05-05 14:39:11 -0400

    Adding first draft of media containing Movie class and entertainment_center containing toy_story object

---
+++ python/udacity/movies/entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://youtube.com/watch?v=vwyZH85NQC4")
+print(toy_story.storyline)
