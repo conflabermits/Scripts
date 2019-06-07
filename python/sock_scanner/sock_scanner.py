33ff4b520107d74f8879ca988d5696832d043916
Chris Dunaj <christopher.dunaj@gmail.com>
2019-06-07 14:33:28 -0400

    Got a working skeleton that takes an argument and does simple if-else to check product existence

--- python/sock_scanner/sock_scanner.py
+++ python/sock_scanner/sock_scanner.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+import argparse
 
-#page = requests.get('https://www.nyandcompany.com/search/?Dy=1&Nty=1&Ntp=1&Ntt=quijibo')
-page = requests.get('https://www.nyandcompany.com/search/?Dy=1&Nty=1&Ntp=1&Ntt=socks')
+parser = argparse.ArgumentParser(description='Scan the NY&C website for products by string')
+parser.add_argument("-q",
+                    "--query",
+                    type=str,
+                    help="String text used to query the NY&C website",
+                    required=True)
+
+args = parser.parse_args()
+
+page = requests.get('https://www.nyandcompany.com/search/?Dy=1&Nty=1&Ntp=1&Ntt={0}'.format(args.query))
 soup = BeautifulSoup(page.content, 'html.parser')
-#soup = BeautifulSoup(page.text, 'html.parser')
 
-print(soup.prettify())
-#print(soup.get_text())
-#print(soup.find(text="We found "))
+if soup.find_all(attrs={"class": "resultFound"}):
+    print("Results found!")
+else:
+    print("No results found")
+
 
