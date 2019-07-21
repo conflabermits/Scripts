45b62174eaf9aff38dd7d1ccebcfba3738b2c8cd
Chris Dunaj <christopher.dunaj@gmail.com>
2019-07-21 00:20:31 -0400

    Improved incoming tweet data and print formatting

--- python/reddit/openargs/get_recent_tweets.py
+++ python/reddit/openargs/get_recent_tweets.py
@@ -6,12 +6,26 @@ from openargs_twitter_creds import *
 api = twitter.Api(consumer_key=twitter_consumer_key,
                   consumer_secret=twitter_consumer_secret,
                   access_token_key=twitter_access_token_key,
-                  access_token_secret=twitter_access_token_secret)
+                  access_token_secret=twitter_access_token_secret,
+                  tweet_mode="extended")
 
 twitter_user = api.VerifyCredentials()
 print("Running as user \"{0}\" (handle: {1})\n".format(twitter_user.name, twitter_user.screen_name))
 
 t = api.GetUserTimeline(screen_name="openargs", exclude_replies=True, count=30)
 tweets = [i.AsDict() for i in t]
+
 for t in tweets:
-    print(t['id'], t['text'])
+    if 'retweeted_status' in t.keys():
+        print(
+            t['created_at'] + "\n" + "RT by @" + t['user']['screen_name'] + " from @" + t['retweeted_status']['user']['screen_name'] + ", " + t['retweeted_status']['created_at'] + ":",
+            "\n",
+            t['retweeted_status']['full_text'],
+            "\n")
+    else:
+        print(
+            t['created_at'] + "\n" + "Tweet from @" + t['user']['screen_name'] + ":",
+            "\n",
+            t['full_text'],
+            "\n")
+
