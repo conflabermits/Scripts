519153230e6aafea4e52d8bd0da0596bcbefae35
conflabermits <christopher.dunaj@gmail.com>
2019-07-25 15:04:59 -0400

    Cleaned up unneeded and unused code, improved naming

--- python/reddit/openargs/openargs_fetchpost.py
+++ python/reddit/openargs/openargs_fetchpost.py
@@ -15,6 +15,11 @@ from openargs_reddit_creds import *
 
 
 parser = argparse.ArgumentParser(description='Grab tweets and retweets from a Twitter account and post to a given subreddit')
+parser.add_argument("-a",
+                    "--account",
+                    help="Account to grab tweets from",
+                    type=str,
+                    required=True)
 parser.add_argument("-s",
                     "--silent",
                     help="Suppress terminal output",
@@ -36,20 +41,14 @@ parser.add_argument("-l",
                     help="Limit results to N tweets (default: %(default)s)",
                     required=False,
                     type=int,
-                    default="30")
+                    default="50")
 parser.add_argument("-H",
                     "--hours",
-                    help="Find results going back <HOURS> hours",
+                    help="How far back, in hours, to search for tweets (default: %(default)s)",
                     type=int,
-                    default="0",
+                    default="24",
                     required=False)
 """
-parser.add_argument("-M",
-                    "--minutes",
-                    help="Find results going back <MINUTES> minutes",
-                    type=int,
-                    default="0",
-                    required=False)
 parser.add_argument("-i",
                     "--include",
                     help="Only include results matching <STRING>",
@@ -61,46 +60,29 @@ parser.add_argument("-e",
                     type=str,
                     required=False)
 """
-parser.add_argument("-a",
-                    "--account",
-                    help="Account to grab tweets from",
-                    type=str,
-                    required=True)
 
 args = parser.parse_args()
 
-twitter_account=args.account
+twitter_account = args.account
+#reddit_subreddit = args.subreddit
 
-# Resolve argument conflicts
 if args.user is None and args.silent is True:
     print("No output method defined. Exiting.\n")
     sys.exit(2)
 
-"""
-if args.hours != 0 and args.minutes != 0:
-    print("Warning: Both hours and minutes specified.")
-    print("They will be combined into a single value.\n")
-
-if args.hours == 0 and args.minutes == 0:
-    args.hours = 336
-"""
-
-
-def to_datetime(datestring):
+def convert_twitter_date_to_date_object(datestring):
     time_tuple = parsedate_tz(datestring.strip())
     dt = datetime(*time_tuple[:6])
     return dt - timedelta(seconds=time_tuple[-1])
 
-DESIRED_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 #TWITTER_DATE_FORMAT = '%a %b %d %H:%M:%S +0000 %Y'
-TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DESIRED_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-def utc_2_local(utc_time):
-    utc = datetime.utcfromtimestamp(utc_time).strftime(TIME_FORMAT)
-    timestamp =  calendar.timegm((datetime.strptime(utc, TIME_FORMAT)).timetuple())
-    local = datetime.fromtimestamp(timestamp).strftime(TIME_FORMAT)
+def convert_utc_2_local(utc_time):
+    utc = datetime.utcfromtimestamp(utc_time).strftime(DESIRED_DATE_FORMAT)
+    timestamp =  calendar.timegm((datetime.strptime(utc, DESIRED_DATE_FORMAT)).timetuple())
+    local = datetime.fromtimestamp(timestamp).strftime(DESIRED_DATE_FORMAT)
     return(local)
-
 
 reddit = praw.Reddit(client_id=reddit_client_id,
                      client_secret=reddit_client_secret,
@@ -108,99 +90,45 @@ reddit = praw.Reddit(client_id=reddit_client_id,
                      password=reddit_password,
                      user_agent='openargs_fetchposter/0.1 by verylegalandcoolbot')
 
-api = twitter.Api(consumer_key=twitter_consumer_key,
+twitter_api = twitter.Api(consumer_key=twitter_consumer_key,
                   consumer_secret=twitter_consumer_secret,
                   access_token_key=twitter_access_token_key,
                   access_token_secret=twitter_access_token_secret,
                   tweet_mode="extended")
 
-twitter_user = api.VerifyCredentials()
+twitter_user = twitter_api.VerifyCredentials()
 
-"""
-print(
-    "Running as user \"{0}\" (handle: {1})\n".format(
-        twitter_user.name,
-        twitter_user.screen_name
-    )
-)
-"""
-
-#postNumber = 1
-#postLimit = args.limit
 redditSeparator = "\n\n---\n\n"
-messageBody = "Generated at: {0}{1}".format(datetime.now().strftime(TIME_FORMAT), redditSeparator)
+messageBody = "Generated at: {0}{1}".format(datetime.now().strftime(DESIRED_DATE_FORMAT), redditSeparator)
 
-t = api.GetUserTimeline(screen_name=twitter_account, exclude_replies=True, count=30)
-tweets = [i.AsDict() for i in t]
+tweet = twitter_api.GetUserTimeline(screen_name=twitter_account, exclude_replies=True, count=args.limit)
+tweets = [i.AsDict() for i in tweet]
 
-for t in tweets:
-    if 'retweeted_status' in t.keys():
+for tweet in tweets:
+    if 'retweeted_status' in tweet.keys():
         messageBody += "RT by @{0} ({1}) of @{2} ({3}):\n\n".format(
-            t['user']['screen_name'],
-            datetime.strftime(to_datetime(t['created_at']), DESIRED_DATE_FORMAT + ' UTC'),
-            t['retweeted_status']['user']['screen_name'],
-            datetime.strftime(to_datetime(t['retweeted_status']['created_at']), DESIRED_DATE_FORMAT + ' UTC')
+            tweet['user']['screen_name'],
+            datetime.strftime(convert_twitter_date_to_date_object(tweet['created_at']), DESIRED_DATE_FORMAT + ' UTC'),
+            tweet['retweeted_status']['user']['screen_name'],
+            datetime.strftime(convert_twitter_date_to_date_object(tweet['retweeted_status']['created_at']), DESIRED_DATE_FORMAT + ' UTC')
         )
         messageBody += textwrap.indent(
-            t['retweeted_status']['full_text'],
+            tweet['retweeted_status']['full_text'],
             "> ",
             lambda line: True
         )
         messageBody += redditSeparator
     else:
         messageBody += "Tweet from @{0} ({1}):\n\n".format(
-            t['user']['screen_name'],
-            datetime.strftime(to_datetime(t['created_at']), DESIRED_DATE_FORMAT + ' UTC')
+            tweet['user']['screen_name'],
+            datetime.strftime(convert_twitter_date_to_date_object(tweet['created_at']), DESIRED_DATE_FORMAT + ' UTC')
         )
         messageBody += textwrap.indent(
-            t['full_text'],
+            tweet['full_text'],
             "> ",
             lambda line: True
         )
         messageBody += redditSeparator
-
-
-"""
-def printPost(submission):
-    global messageBody
-    messageBody += "**Post Number:** {0}\n\n".format(postNumber)
-    messageBody += "**Post Title:** {0}\n\n".format(submission.title)
-    messageBody += "**Post Date:** {0}\n\n".format(utc_2_local(submission.created_utc))
-    messageBody += "**Link To Post:** {0}\n\n".format(submission.shortlink)
-    if submission.is_self:
-        messageBody += "**Post Selftext:**\n\n"
-        messageBody += textwrap.indent(submission.selftext,
-                                       "> ",
-                                       lambda line: True)
-    else:
-        messageBody += "**External URL:** {0}\n".format(submission.url)
-    messageBody += redditSeparator
-"""
-
-"""
-for submission in reddit.subreddit(args.subreddit).new(limit=postLimit):
-    dateGate = datetime.now() - datetime.timedelta(hours = args.hours, minutes = args.minutes) <= datetime.fromtimestamp(submission.created_utc)
-    if dateGate:
-        if args.include is None and args.exclude is None:
-            printPost(submission)
-            postNumber += 1
-        else:
-            if args.include and args.exclude:
-                if (re.search(args.include, submission.title, re.IGNORECASE) or re.search(args.include, submission.selftext, re.IGNORECASE)):
-                    if not (re.search(args.exclude, submission.title, re.IGNORECASE) or re.search(args.exclude, submission.selftext, re.IGNORECASE)):
-                        printPost(submission)
-                        postNumber += 1
-            if args.exclude is None and args.include:
-                if (re.search(args.include, submission.title, re.IGNORECASE) or re.search(args.include, submission.selftext, re.IGNORECASE)):
-                    printPost(submission)
-                    postNumber += 1
-            if args.include is None and args.exclude:
-                if not (re.search(args.exclude, submission.title, re.IGNORECASE) or re.search(args.exclude, submission.selftext, re.IGNORECASE)):
-                    printPost(submission)
-                    postNumber += 1
-    else:
-        break
-"""
 
 if args.silent is False:
     print(messageBody)
@@ -208,4 +136,3 @@ if args.silent is False:
 if args.user is not None:
     print("Sending message to /u/{0}".format(args.user))
     reddit.redditor(args.user).message("Most recent tweets from @{0}".format(twitter_account), messageBody)
-
