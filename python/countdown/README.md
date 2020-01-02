3063c7be90ca11e92e5fbbf8b24010068c14b7d4
chris <christopher.dunaj@gmail.com>
2020-01-02 15:12:15 -0500

    Adding rudimentary iteration logic for biggestword puzzle

--- python/countdown/README.md
+++ python/countdown/README.md
@@ -1,7 +1,9 @@
+## Countdown Tools
+
 Helpful tools for solving Countdown's problems
 
 Sources:
-https://github.com/dwyl/english-words/blob/master/words_alpha.txt
-https://github.com/dwyl/english-words/blob/master/words_dictionary.json
-https://github.com/dwyl/english-words/blob/master/read_english_dictionary.py
+* https://github.com/dwyl/english-words/blob/master/words_alpha.txt
+* https://github.com/dwyl/english-words/blob/master/words_dictionary.json
+* https://github.com/dwyl/english-words/blob/master/read_english_dictionary.py
 
--- python/countdown/conundrum.py
+++ python/countdown/conundrum.py
@@ -12,6 +12,7 @@ parser.add_argument("-l",
 parser.add_argument("-t",
                     "--gametype",
                     type=str,
+                    choices=['biggestword', 'conundrum'],
                     help="Either biggestword or conundrum",
                     required=True)
 
@@ -30,15 +31,65 @@ def solve_conundrum(letters, words):
                 valid_answers.append(word)
     return valid_answers
 
+def get_permutations(permutations):
+    given_permutations = list()
+    new_permutations = list()
+    if permutations is list:
+        for permutation in permutations:
+            given_permutations.append(permutation)
+    else:
+        given_permutations.append(permutations)
+    for permutation in given_permutations:
+        for i in range(0, len(permutation)):
+            removal_letter = sorted(permutation).pop(i)
+            sorted_permutation = sorted(permutation)
+            sorted_permutation.remove(removal_letter)
+            potential_new_permutation = ''.join(map(str, sorted_permutation))
+            if potential_new_permutation not in new_permutations:
+                new_permutations.append(potential_new_permutation)
+    return new_permutations
+
+
 if __name__ == '__main__':
-    if args.gametype not in ['biggestword', 'conundrum']:
-        print('Please enter either biggestword or conundrum as the game type')
-        sys.exit(1)
+
+    # Assign variables for ease of re-use
+    input_letters = args.letters
+    input_gametype = args.gametype
+
+    # Ensure input is only alpha
+    if not input_letters.isalpha():
+        print('Please use only letters A through Z, no numbers or symbols')
+        sys.exit(2)
+
+    # Ensure letters are all lowercase
+    if not input_letters.islower():
+        tmp_letters = input_letters.lower()
+        input_letters = tmp_letters
+
+    ## Ensure gametype is selected properly
+    #if input_gametype not in ['biggestword', 'conundrum']:
+    #    print('Please enter either biggestword or conundrum as the game type')
+    #    sys.exit(1)
 
     english_words = load_words()
-    print('Length of english_words: {0}'.format(len(english_words)))
-    potential_answers = solve_conundrum(args.letters, english_words)
-    print('Length of potential_answers: {0}'.format(len(potential_answers)))
-    for word in potential_answers:
-        print(word)
+
+    if input_gametype == "conundrum":
+        potential_answers = solve_conundrum(input_letters, english_words)
+        print('Length of potential_answers: {0}'.format(len(potential_answers)))
+        for word in potential_answers:
+            print(word)
+
+    if input_gametype == "biggestword":
+        potential_answers = solve_conundrum(input_letters, english_words)
+        old_combo = input_letters
+        while len(potential_answers) == 0:
+            new_letter_combos = get_permutations(old_combo)
+            for combo in new_letter_combos:
+                new_potential_answers = solve_conundrum(combo, english_words)
+                for potential_answer in new_potential_answers:
+                    if potential_answer not in potential_answers:
+                        potential_answers.append(potential_answer)
+            old_combo = new_letter_combos
+        for word in potential_answers:
+            print(word)
 
