f66f54e7cf5b7877f8643808cb66c6f281125ee8
chris <christopher.dunaj@gmail.com>
2020-02-09 12:43:04 -0500

    week 2 polysum optional exercise

--- python/edx/scratch.py
+++ python/edx/scratch.py
@@ -1,25 +1,8 @@
 #!/usr/bin/env python3
 
-def isIn(char, aStr):
-    '''
-    char: a single character
-    aStr: an alphabetized string
-    
-    returns: True if char is in aStr; False otherwise
-    '''
-    # Your code here
-    if len(aStr) == 0:
-        return False
-    if len(aStr) == 1:
-        return char == aStr
-    
-    midChar = aStr[(len(aStr)//2)]
-    if char == midChar:
-        return True
-    else:
-        if char < midChar:
-            newStr = aStr[0:(len(aStr)//2)]
-        else:
-            newStr = aStr[(len(aStr)//2):]
-        return isIn(char,newStr)
+from math import *
 
+def polysum(n, s):
+    area = (0.25*n*(s**2))/tan(pi/n)
+    perimeter = s * n
+    return round(area + perimeter**2, 4)
