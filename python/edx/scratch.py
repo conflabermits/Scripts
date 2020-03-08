8c30080acfd2cfa0c049311510927e508159e999
chris <christopher.dunaj@gmail.com>
2020-03-08 11:07:42 -0400

    Example of __eq__ and __repr__ in a class

--- python/edx/scratch.py
+++ python/edx/scratch.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
-def avg(grades):
-    try:
-        assert not len(grades) == 0
-    except AssertionError:
-        print('No grades entered')
-        return 0
-    return sum(grades)/len(grades)
+class Coordinate(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+    def __eq__(self, other):
+        return Coordinate.getX(self) == Coordinate.getX(other) and Coordinate.getY(self) == Coordinate.getY(other)
+    def __repr__(self):
+        return 'Coordinate({0},{1})'.format(self.getX(),self.getY())
