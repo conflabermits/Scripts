06cdd8d410d516e4a3d66bac3a573cc1776a1306
chris <christopher.dunaj@gmail.com>
2020-05-25 22:53:15 -0400

    Did some testing, added some notes for shelving

--- python/xps_price_checker/xps9500_price_scanner.py
+++ python/xps_price_checker/xps9500_price_scanner.py
@@ -9,6 +9,35 @@ import price_history
 # Base URL: https://www.dell.com/en-us/member/shop/dell-laptops/new-xps-15-laptop/spd/xps-15-9500-laptop/xn9500cto210s
 # URL with 512GB SSD: https://www.dell.com/en-us/member/shop/dell-laptops/new-xps-15-laptop/spd/xps-15-9500-laptop/xn9500cto210s?configurationid=62e03267-650b-4469-be2d-38eda0ba102b
 
+# TO DO
+# implement price history file
+'''
+$ python3
+Python 3.6.9 (default, Nov  7 2019, 10:44:02)
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import shelve
+>>> price_history = shelve.open('price_history')
+>>> print(price_history['previous_price'])
+1714.99
+>>> price_history['previous_price'] = 1749.99
+>>> print(price_history['previous_price'])
+1749.99
+>>> price_history.close()
+'''
+# make file check
+'''
+$ python3
+Python 3.6.9 (default, Nov  7 2019, 10:44:02)
+[GCC 8.3.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import os
+>>> os.path.isfile('price_history')
+True
+'''
+# make functions, main block, etc.
+# add notification system (email/reddit)
+
 parser = argparse.ArgumentParser(description="Check the price of the Dell XPS 15 9500")
 parser.add_argument("-u",
                     "--url",
