41e634a66eb44187c7845b7e7a53dfddb045c4f6
chris <christopher.dunaj@gmail.com>
2020-08-07 15:24:18 -0400

    Adding functional sample script and notes

--- python/lambda-ses-testing/amazon-ses-sample.py
+++ python/lambda-ses-testing/amazon-ses-sample.py
@@ -4,21 +4,23 @@
 import boto3
 from botocore.exceptions import ClientError
 
+from ses_sample_variables import *
+
 # Replace sender@example.com with your "From" address.
 # This address must be verified with Amazon SES.
-SENDER = "Sender Name <sender@example.com>"
+SENDER = from_email
 
 # Replace recipient@example.com with a "To" address. If your account
 # is still in the sandbox, this address must be verified.
-RECIPIENT = "recipient@example.com"
+RECIPIENT = to_email
 
 # Specify a configuration set. If you do not want to use a configuration
 # set, comment the following variable, and the
 # ConfigurationSetName=CONFIGURATION_SET argument below.
-CONFIGURATION_SET = "ConfigSet"
+#CONFIGURATION_SET = "ConfigSet"
 
 # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-AWS_REGION = "us-west-2"
+AWS_REGION = region
 
 # The subject line for the email.
 SUBJECT = "Amazon SES Test (SDK for Python)"
@@ -76,7 +78,7 @@ try:
         Source=SENDER,
         # If you are not using a configuration set, comment or delete the
         # following line
-        ConfigurationSetName=CONFIGURATION_SET,
+        #ConfigurationSetName=CONFIGURATION_SET,
     )
 # Display an error if something goes wrong.
 except ClientError as e:
