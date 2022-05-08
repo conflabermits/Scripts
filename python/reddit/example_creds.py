5172a143e9c9c728e20da336e52c26b4d02caf55
chris <christopher.dunaj@gmail.com>
2022-05-07 21:34:51 -0400

    Just adding a basic example of a credentials file

---
+++ python/reddit/example_creds.py
@@ -0,0 +1,7 @@
+# My variables and values:
+
+reddit_client_id = 'abc123def456gh'
+reddit_client_secret = '123abc456def_-!789ghi012jkl'
+reddit_username = 'example_username'
+reddit_password = 'a1b2c3d4e5f6_-+=!@#$%^&*(g7h8i9'
+
