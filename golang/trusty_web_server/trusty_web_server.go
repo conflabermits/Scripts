6a559694f6bb44bd08baa3687b881394d8ac4d91
chris <christopher.dunaj@gmail.com>
2022-06-19 14:42:37 -0400

    Added json responses to ok, outage, degraded paths

--- golang/trusty_web_server/trusty_web_server.go
+++ golang/trusty_web_server/trusty_web_server.go
@@ -7,62 +7,149 @@ import (
 )
 
 func respond_ok(w http.ResponseWriter, req *http.Request) {
-
-    fmt.Fprintf(w, "OK\n")
+	ok_text := `{
+        "components": [
+          {
+            "description": "Most important check",
+            "essential": true,
+            "name": "auth-service",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:38080/auth-service/health"
+          },
+          {
+            "description": "Less important check",
+            "essential": false,
+            "name": "activity-webservice",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:38080/activity-service/health"
+          },
+          {
+            "description": "Some other cheeck",
+            "essential": true,
+            "name": "database",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:48080/user-table"
+          }
+        ],
+        "name": "appname",
+        "statusCode": "OK"
+      }`
+	fmt.Fprintf(w, ok_text)
 }
 
 func respond_degraded(w http.ResponseWriter, req *http.Request) {
-
-    fmt.Fprintf(w, "Degraded\n")
+	degraded_text := `{
+        "components": [
+          {
+            "description": "Most important check",
+            "essential": true,
+            "name": "auth-service",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:38080/auth-service/health"
+          },
+          {
+            "description": "Less important check",
+            "essential": false,
+            "name": "activity-webservice",
+            "statusCode": "CRITICAL",
+            "statusText": "Can't reach activity service, returns 404",
+            "uri": "http://localhost:38080/activity-service/health"
+          },
+          {
+            "description": "Some other cheeck",
+            "essential": true,
+            "name": "database",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:48080/user-table"
+          }
+        ],
+        "name": "appname",
+        "statusCode": "DEGRADED"
+      }`
+	fmt.Fprintf(w, degraded_text)
 }
 
 func respond_outage(w http.ResponseWriter, req *http.Request) {
-
-    fmt.Fprintf(w, "Outage\n")
+	outage_text := `{
+        "components": [
+          {
+            "description": "Most important check",
+            "essential": true,
+            "name": "auth-service",
+            "statusCode": "CRITICAL",
+            "statusText": "Can't reach auth service, returns 500",
+            "uri": "http://localhost:38080/auth-service/health"
+          },
+          {
+            "description": "Less important check",
+            "essential": false,
+            "name": "activity-webservice",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:38080/activity-service/health"
+          },
+          {
+            "description": "Some other cheeck",
+            "essential": true,
+            "name": "database",
+            "statusCode": "OK",
+            "statusText": null,
+            "uri": "http://localhost:48080/user-table"
+          }
+        ],
+        "name": "appname",
+        "statusCode": "OUTAGE"
+      }`
+	fmt.Fprintf(w, outage_text)
 }
 
 func respond_200(w http.ResponseWriter, req *http.Request) {
 
-    fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, "\n")
 }
 
 func respond_401(w http.ResponseWriter, req *http.Request) {
 
-    w.WriteHeader(401)
-    fmt.Fprintf(w, "Not authorized\n")
+	w.WriteHeader(401)
+	fmt.Fprintf(w, "Not authorized\n")
 }
 
 func respond_404(w http.ResponseWriter, req *http.Request) {
 
-    w.WriteHeader(404)
-    fmt.Fprintf(w, "Page not found\n")
+	w.WriteHeader(404)
+	fmt.Fprintf(w, "Page not found\n")
 }
 
 func respond_500(w http.ResponseWriter, req *http.Request) {
 
-    w.WriteHeader(500)
-    fmt.Fprintf(w, "Internal server error\n")
+	w.WriteHeader(500)
+	fmt.Fprintf(w, "Internal server error\n")
 }
 
 func respond_301(w http.ResponseWriter, req *http.Request) {
 
-    http.Redirect(w, req, "/200", 301)
+	http.Redirect(w, req, "/200", 301)
 
 }
 
 func respond_302(w http.ResponseWriter, req *http.Request) {
 
-    http.Redirect(w, req, "/ok", 302)
+	http.Redirect(w, req, "/ok", 302)
 
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
 
-    for name, headers := range req.Header {
-        for _, h := range headers {
-            fmt.Fprintf(w, "%v: %v\n", name, h)
-        }
-    }
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
 }
 
 func main() {
@@ -70,18 +157,18 @@ func main() {
 	port := flag.String("port", "8080", "Port to run the local web server")
 	flag.Parse()
 
-    http.HandleFunc("/ok", respond_ok)
-    http.HandleFunc("/degraded", respond_degraded)
-    http.HandleFunc("/outage", respond_outage)
+	http.HandleFunc("/ok", respond_ok)
+	http.HandleFunc("/degraded", respond_degraded)
+	http.HandleFunc("/outage", respond_outage)
 
-    http.HandleFunc("/200", respond_200)
-    http.HandleFunc("/301", respond_301)
-    http.HandleFunc("/302", respond_302)
-    http.HandleFunc("/401", respond_401)
-    http.HandleFunc("/404", respond_404)
-    http.HandleFunc("/500", respond_500)
+	http.HandleFunc("/200", respond_200)
+	http.HandleFunc("/301", respond_301)
+	http.HandleFunc("/302", respond_302)
+	http.HandleFunc("/401", respond_401)
+	http.HandleFunc("/404", respond_404)
+	http.HandleFunc("/500", respond_500)
 
-    http.HandleFunc("/headers", headers)
+	http.HandleFunc("/headers", headers)
 
-    http.ListenAndServe(":"+*port, nil)
+	http.ListenAndServe(":"+*port, nil)
 }
