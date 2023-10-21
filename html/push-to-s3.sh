ed96f50a3692d98b5c3ace01f27713fff8e3ebe2
conflabermits <christopher.dunaj@gmail.com>
2023-10-21 13:04:17 -0400

    Updates to s3 sync to support any bucket & source

--- html/push-to-s3.sh
+++ html/push-to-s3.sh
@@ -3,9 +3,39 @@
 # Set errors var
 errors="false"
 
+default_creds="~/.aws/credentials"
+
+while getopts ":s:b:c:" opt; do
+  case $opt in
+    s)
+      SOURCE="$OPTARG"
+      ;;
+    b)
+      BUCKET="$OPTARG"
+      ;;
+    c)
+      CREDS="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ -z "${SOURCE}" ]] || [[ -z "${BUCKET}" ]]; then
+    echo "The following arguments are required: -s <SOURCE> -b <BUCKET>"
+    exit 1
+fi
+
 # Check for credentials in expected location
-if [ -f ~/.aws/credentials ]; then
+if [ -f ${CREDS} ]; then
     echo "Credentials found"
+    export AWS_CONFIG_FILE=${CREDS}
 else
     echo "NO CREDENTIALS FOUND"
     errors="true"
@@ -19,19 +49,19 @@ else
     errors="true"
 fi
 
-# Check for remote S3 chrisdunaj.com bucket successful connection
-if aws s3 ls s3://chrisdunaj.com >/dev/null; then
-    echo "Connection to AWS and S3 chrisdunaj.com bucket successful"
+# Check for remote S3 ${BUCKET} bucket successful connection
+if aws s3 ls s3://${BUCKET}/ >/dev/null; then
+    echo "Connection to AWS and S3 ${BUCKET} bucket successful"
 else
-    echo "CONNECTION TO \"chrisdunaj.com\" BUCKET OR AWS UNSUCCESSFUL"
+    echo "CONNECTION TO \"${BUCKET}\" BUCKET OR AWS UNSUCCESSFUL"
     errors="true"
 fi
 
-# Check for local chrisdunaj.com directory in current directory
-if [ -d ./chrisdunaj.com ]; then
-    echo "Directory chrisdunaj.com found"
+# Check for local ${SOURCE} directory in current directory
+if [ -d ./${SOURCE} ]; then
+    echo "Directory ${SOURCE} found"
 else
-    echo "NO \"chrisdunaj.com\" DIRECTORY FOUND";
+    echo "NO \"${SOURCE}\" DIRECTORY FOUND";
     errors="true"
 fi
 
@@ -40,6 +70,6 @@ if [ errors == "false" ]; then
     echo "Please fix above errors and try again"
     exit 1
 else
-    echo -e "\n\n === COPYING chrisdunaj.com TO s3://chrisdunaj.com === \n\n"
-    aws s3 sync chrisdunaj.com s3://chrisdunaj.com
+    echo -e "\n\n === COPYING ${SOURCE} TO s3://${BUCKET} === \n\n"
+    aws s3 sync ${SOURCE} s3://${BUCKET}
 fi
